d3b2c76dd1f0e2fb35c23e0edc88f3230ad12391
mark-oblack <moblack42@gmail.com>
2019-02-21 17:31:03 -0500

    Added return to top button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 $(document).foundation()
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("return-to-top").style.display = "block";
+  } else {
+    document.getElementById("return-to-top").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
